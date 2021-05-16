26cc6782aee38427f4c07266021fa5ecb0ba8895
tviteritti <tviteritti@alumno.unlam.edu.ar>
2021-05-16 08:33:09 -0300

    nav de boostrap

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorIndex.java
@@ -0,0 +1,18 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+@Controller
+public class ControladorIndex {
+
+	@RequestMapping("/index")
+	public ModelAndView irIndex() {
+
+		return new ModelAndView("index");
+	}
+}
