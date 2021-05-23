235d5c9e82255c4a952797bc594411f1148d5b8f
julieta96 <julietaextra14@gmail.com>
2021-05-23 03:11:00 -0300

    modificacion en buscarServicioVeterinario + se agrego servicio

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorTurnos.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorTurnos.java
@@ -1,15 +1,36 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unlam.tallerweb1.servicios.ServicioLoginVeterinaria;
+import ar.edu.unlam.tallerweb1.servicios.ServicioTurno;
 
 @Controller
 public class ControladorTurnos {
 	
+private ServicioTurno servicio;
+
+	
+	@Autowired
+	public ControladorTurnos(ServicioTurno servicioTurno) {
+		
+		this.servicio = servicioTurno;	
+	}
+	
 	@RequestMapping("buscarTurno")
-	public ModelAndView buscarServicioVeterinario() {
+	public ModelAndView buscarServicioVeterinario(
+			@RequestParam(value="zona",required=false) String zona) {
+		ModelMap modelo = new ModelMap();
+		if(zona!=null)
+		{
+			modelo.put("localidades", servicio.obtenerLocalidades(zona));
+			return new ModelAndView("buscarTurno" , modelo);
+		}
 		
 		return new ModelAndView("buscarTurno");
 	}
