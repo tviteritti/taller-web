d39b8405ef71e1fbdeffd8f0c09479a7399aa585
julieta96 <julietaextra14@gmail.com>
2021-05-23 03:22:25 -0300

    repositorio localidad

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioLocalidad.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.Localidad;
+import ar.edu.unlam.tallerweb1.modelo.Zona;
+
+public interface RepositorioLocalidad {
+	
+	List<Localidad> obtenerLocalidades(String zona);
+
+}
