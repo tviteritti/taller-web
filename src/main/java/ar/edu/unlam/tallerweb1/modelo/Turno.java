324a1349a63a35d0f95987c9582fcb7e5bb83229
julieta96 <julietaextra14@gmail.com>
2021-05-24 19:13:03 -0300

    modelo turno

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Turno.java
@@ -0,0 +1,66 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Turno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	private Veterinario veterinario;
+	@ManyToOne
+	private Paciente paciente;
+	
+	private String fecha;
+	private String horario;
+	private String servicio;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Veterinario getVeterinario() {
+		return veterinario;
+	}
+	public void setVeterinario(Veterinario veterinario) {
+		this.veterinario = veterinario;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public String getServicio() {
+		return servicio;
+	}
+	public void setServicio(String servicio) {
+		this.servicio = servicio;
+	}
+	
+	
+	
+	
+	
+
+}
