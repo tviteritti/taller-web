0b6babb4fad8efed8733bd609d62b6dafdfab99a
julieta96 <julietaextra14@gmail.com>
2021-05-24 01:58:47 -0300

    cambios en buscarServicioVeterinario + requestparam en los metodos

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorTurnos.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/ControladorTurnos.java
@@ -3,10 +3,13 @@ package ar.edu.unlam.tallerweb1.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+import ar.edu.unlam.tallerweb1.modelo.Zona;
 import ar.edu.unlam.tallerweb1.servicios.ServicioLoginVeterinaria;
 import ar.edu.unlam.tallerweb1.servicios.ServicioTurno;
 
@@ -23,35 +26,43 @@ private ServicioTurno servicio;
 	}
 	
 	@RequestMapping("buscarTurno")
-	public ModelAndView buscarServicioVeterinario(
-			@RequestParam(value="zona",required=false) String zona) {
+	public ModelAndView buscarServicioVeterinario() {
 		ModelMap modelo = new ModelMap();
-		if(zona!=null)
-		{
-			modelo.put("localidades", servicio.obtenerLocalidades(zona));
-			return new ModelAndView("buscarTurno" , modelo);
-		}
 		
-		return new ModelAndView("buscarTurno");
+		Zona zona = new Zona();
+		modelo.put("zona", zona);
+		return new ModelAndView("buscarTurno", modelo);
 	}
 
 	@RequestMapping("buscarServicioVeterinario")
-	public ModelAndView mostrarServicioVeterinario() {
+	public ModelAndView mostrarServicioVeterinario(
+			
+			@RequestParam(value="servicio",required=false) String servicioSolicitado,
+			@ModelAttribute("zona") Zona zona) {
 		
-		return new ModelAndView("servicioVeterinario");
+		ModelMap modelo = new ModelMap();
+		modelo.put("servicio", servicioSolicitado);
+		modelo.put("zona", zona);
+		
+		return new ModelAndView("servicioVeterinario", modelo);
 	}
 	
 	@RequestMapping("generarTurno")
-	public ModelAndView mostrarTurnoSolicitado() {
+	public ModelAndView mostrarTurnoSolicitado(
+	@RequestParam(value="servicio",required=false) String servicioSolicitado,
+	@RequestParam(value="fecha",required=false) String dia,
+	@RequestParam(value="hora",required=false) String hora
+	
+			) {
 		
 		ModelMap modelo = new ModelMap();
 		
 		modelo.put("veterinario", "Nombre Veterinario");
-		modelo.put("especialidad", "Especialidad Veterinaro");
+		modelo.put("especialidad", servicioSolicitado);
 		modelo.put("localidad", "Localidad Veterinaria");
 		modelo.put("direccion", "Direccion Veterinaria");
-		modelo.put("fecha", "fecha seleccionada");
-		modelo.put("hora", "horario seleccionado");
+		modelo.put("fecha", dia);
+		modelo.put("hora", hora);
 		
 		return new ModelAndView("turnoSolicitado", modelo);
 	}
