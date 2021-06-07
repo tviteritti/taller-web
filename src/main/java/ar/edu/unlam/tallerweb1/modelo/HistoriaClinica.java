5ee5a678c81630cbc0bfa3bde2f493a5ebdb2c67
julieta96 <julietaextra14@gmail.com>
2021-06-07 04:27:33 -0300

    mascota + veterinario

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/HistoriaClinica.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/HistoriaClinica.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class HistoriaClinica {
@@ -12,11 +13,17 @@ public class HistoriaClinica {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	/*private Veterinario veterinario;*/
+
 	private String fecha;
 	private String diagnostico;
 	private String tratamiento;
 	
+    @ManyToOne
+    private Mascota mascota;
+    
+    @ManyToOne
+	private Usuario veterinario;
+    
 	public Long getId() {
 		return id;
 	}
@@ -42,5 +49,19 @@ public class HistoriaClinica {
 	public void setTratamiento(String tratamiento) {
 		this.tratamiento = tratamiento;
 	}
+	public Mascota getMascota() {
+		return mascota;
+	}
+	public void setMascota(Mascota mascota) {
+		this.mascota = mascota;
+	}
+	public Usuario getVeterinario() {
+		return veterinario;
+	}
+	public void setVeterinario(Usuario veterinario) {
+		this.veterinario = veterinario;
+	}
+	
+	
 
 }
