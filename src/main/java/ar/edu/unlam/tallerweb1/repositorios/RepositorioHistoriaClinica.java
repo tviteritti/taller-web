c5ddd6a015f9060d0bc54624fc3b65f85424c4cd
julieta96 <julietaextra14@gmail.com>
2021-06-07 04:21:59 -0300

    respositorio hc

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioHistoriaClinica.java
@@ -0,0 +1,15 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.HistoriaClinica;
+import ar.edu.unlam.tallerweb1.modelo.Mascota;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+public interface RepositorioHistoriaClinica {
+	
+	List<HistoriaClinica> listarHistoriaClinicaPorMascotaYVeterinario(Mascota mascota, Usuario veterinario);
+	List<HistoriaClinica> obtenerHistoriaClinica();
+	void cargarHC(Usuario veterinario, Mascota mascota, String fecha, String diagnostico, String tratamiento);
+
+}
---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioHistoriaClinicaImpl.java
@@ -0,0 +1,73 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import ar.edu.unlam.tallerweb1.modelo.HistoriaClinica;
+import ar.edu.unlam.tallerweb1.modelo.Mascota;
+import ar.edu.unlam.tallerweb1.modelo.Usuario;
+
+@Repository
+public class RepositorioHistoriaClinicaImpl implements RepositorioHistoriaClinica{
+	
+	private SessionFactory sessionFactory;
+
+    @Autowired
+	public RepositorioHistoriaClinicaImpl(SessionFactory sessionFactory){
+		this.sessionFactory = sessionFactory;
+	}
+
+
+	@Override
+	public List<HistoriaClinica> listarHistoriaClinicaPorMascotaYVeterinario(Mascota mascota, Usuario veterinario) {
+		
+		List<HistoriaClinica> mascotasHC = (List<HistoriaClinica>) sessionFactory.getCurrentSession()
+				 .createCriteria(HistoriaClinica.class)
+				 .createAlias("mascota", "mBuscada")
+				 .createAlias("veterinario", "vBuscado")
+				 .add(Restrictions.eq("mBuscada.id", mascota.getId()))
+				 .add(Restrictions.eq("vBuscado.id",veterinario.getId()))
+				 .add(Restrictions.eq("vBuscado.rol","veterinario"))
+				 .add((Criterion) Projections.groupProperty("id"))
+				 .list();
+		return mascotasHC;
+	}
+
+
+	@Override
+	public List<HistoriaClinica> obtenerHistoriaClinica() {
+		List<HistoriaClinica> hc = (List<HistoriaClinica>) sessionFactory.getCurrentSession()
+				 .createCriteria(HistoriaClinica.class)
+				 .list();
+		return hc;
+	}
+
+
+	@Override
+	public void cargarHC(Usuario veterinario, Mascota mascota, String fecha, String diagnostico, String tratamiento) {
+		
+		HistoriaClinica hc = new HistoriaClinica();
+		hc.setMascota(mascota);
+		hc.setVeterinario(veterinario);
+		hc.setFecha(fecha);
+		hc.setDiagnostico(diagnostico);
+		hc.setTratamiento(tratamiento);
+		
+		sessionFactory.getCurrentSession().save(hc);
+		
+	}
+	
+	
+	
+	
+	
+	 
+	
+
+}
