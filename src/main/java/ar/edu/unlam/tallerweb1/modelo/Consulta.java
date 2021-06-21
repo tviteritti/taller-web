d614daaddca4020124c000cc129b179958362422
julieta96 <julietaextra14@gmail.com>
2021-06-21 00:32:40 -0300

    modelo consulta

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Consulta.java
@@ -0,0 +1,56 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Consulta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String asunto;
+	private String descripcion;
+	
+	@ManyToOne
+	private Usuario duenio;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Usuario getDuenio() {
+		return duenio;
+	}
+
+	public void setDuenio(Usuario duenio) {
+		this.duenio = duenio;
+	}
+	
+	
+	
+}
